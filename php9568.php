59cc40b075219bbc9dfbcaaee263e6a2e391c44f
refa9568 <107625036+refa9568@users.noreply.github.com>
2022-06-16 18:27:15 +0600

    Add files via upload

---
+++ php9568.php
@@ -0,0 +1,24 @@
+<?php 
+$msg= "hello world";
+echo $msg;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>hello<?php echo $msg;?></h1>
+    <?php 'hello from mist';
+    // arrays
+    foreach ($students as $name => $roll) 
+    {
+        # code...
+    }
+    
+</body>
+
+</html>
