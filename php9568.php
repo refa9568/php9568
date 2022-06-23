b4cb538e6546c029d50fd7dbc6df88d6ce373453
refa9568 <107625036+refa9568@users.noreply.github.com>
2022-06-23 18:34:32 +0600

    Add files via upload

--- php9568.php
+++ php9568.php
@@ -19,6 +19,70 @@ echo $msg;
         # code...
     }
     
+?>
+    <form action='welcome.php'>
+    
+    </form>
+    <input type=''email'' name=''email''><br><br>
+    <input type= "submit" value="submit">
+    </form>
+    </body>
+    $email = $_REQUEST['EMAIL'];
+    
+    echo "email : " . $email; 
+    <form action="post.php" method="post">
+    <label for="name">name</label>
+    <input type="i am here" name="php9568"
+    placeholder="REFA">
+    <br>
+    <input type="submit" value="58"
+    
+    susername = $_POST[ 'refa'];
+    
+    echo 'refa :' .susername
+    <?php
+    echo "today is friday" . date("2022/06/17") . "<br>";
+    echo "today is friday" . date("2022.06.17") . "<br>";
+    echo "today is friday" . date("2022-06-17") . "<br>";
+    ?>
+    </body>
+    </html>
+    <?php
+    date_default_timezone_set('Bangladesh');
+    echo date('2022-06-17 09.05.17');
+    ?>
+    <?php include 'hello from footer page'?>  
 </body>
+<?php
+CLASS Connection{
+    public $server,$username,$password;
+
+    public function _construct ($server, $username, $password)
+    {
+        $this->SERVER = $server;
+        $this->username = $username;
+        $this->password = $password;
+
+    }
+{
+    public function connectDatabase()
+
+    $connection = new mysqli 
+   ( $this->server, 
+   $this->username,
+   $this->passsword
+);
+
+if($connection->connect_error){
+    die('connection failed : ' . $connection)
+    
+    $connection->close();
+    
+}
+
+
+}
+}
 
 </html>
+ 
